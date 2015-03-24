edfd3ba039c4e189cb63facf4c7ab3653382a649
Gregor Weber <gregorwbr@gmail.com>
2015-03-24 14:59:53 +0100

    Switched to a different gulpfile to speed up watch process

--- gulpfile.js
+++ gulpfile.js
@@ -1,107 +1,137 @@
-var gulp        = require('gulp');
+var gulp       = require('gulp');
+var $          = require('gulp-load-plugins')();
+var sync       = $.sync(gulp).sync;
+var del        = require('del');
+var browserify = require('browserify');
+var watchify   = require('watchify');
+var source     = require('vinyl-source-stream');
+var path       = require('path');
 
-var $           = require('gulp-load-plugins')();
-var del         = require('del');
-var source      = require('vinyl-source-stream');
-var browserify  = require('browserify');
-var babelify    = require("babelify");
-var runSequence = require('run-sequence');
+require('harmonize')();
 
-var env = 'dev';
+var bundler = {
+  w: null,
+  init: function() {
+    this.w = watchify(browserify({
+	    extensions: ['.jsx'],
+      entries: ['./app/scripts/app.js'],
+      insertGlobals: true,
+      cache: {},
+      packageCache: {}
+    }));
+  },
+  bundle: function() {
+    return this.w && this.w.bundle()
+      .on('error', $.util.log.bind($.util, 'Browserify Error'))
+      .pipe(source('app.js'))
+      .pipe(gulp.dest('dist/scripts'));
+  },
+  watch: function() {
+    this.w && this.w.on('update', this.bundle.bind(this));
+  },
+  stop: function() {
+    this.w && this.w.close();
+  }
+};
 
-gulp.task('clean:dev', function() {
-  return del(['.tmp']);
-});
-
-gulp.task('clean:dist', function() {
-  return del(['dist']);
+gulp.task('styles', function() {
+	return gulp.src('app/styles/main.less')
+		.pipe($.plumber())
+		.pipe($.less())
+		.pipe(gulp.dest('dist/styles'));
 });
 
 gulp.task('scripts', function() {
-  var bundler = browserify('./app/scripts/app.js', {
-    extensions: ['.jsx'],
-    debug: env == 'dev'
-  }).transform('reactify').transform(babelify);
-
-  return bundler.bundle()
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('.tmp/scripts'));
+  bundler.init();
+  return bundler.bundle();
 });
 
-gulp.task('styles', function() {
-  return gulp.src('app/styles/main.less')
-    .pipe($.plumber())
-    .pipe($.less())
-    .pipe(gulp.dest('.tmp/styles'));
-});
-
-gulp.task('imagemin', function() {
-  return gulp.src('app/images/*')
-    .pipe($.imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}]
-    }))
-    .pipe(gulp.dest('dist/images'));
-});
-
-gulp.task('copy', function() {
-  return gulp.src(['app/*.txt', 'app/*.ico'])
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('bundle', function () {
-  var assets = $.useref.assets({searchPath: '{.tmp,app}'});
-  var jsFilter = $.filter(['**/*.js']);
-  var cssFilter = $.filter(['**/*.css']);
-  var htmlFilter = $.filter(['*.html']);
-
+gulp.task('html', function() {
+  var assets = $.useref.assets();
   return gulp.src('app/*.html')
     .pipe(assets)
     .pipe(assets.restore())
     .pipe($.useref())
-    .pipe(jsFilter)
-    .pipe($.uglify())
-    .pipe(jsFilter.restore())
-    .pipe(cssFilter)
-    .pipe($.autoprefixer({
-      browsers: ['last 5 versions']
-    }))
-    .pipe($.minifyCss())
-    .pipe(cssFilter.restore())
-    .pipe(htmlFilter)
-    .pipe($.htmlmin({collapseWhitespace: true}))
-    .pipe(htmlFilter.restore())
-    .pipe($.revAll({ ignore: [/^\/favicon.ico$/g, '.html'] }))
-    .pipe($.revReplace())
     .pipe(gulp.dest('dist'))
     .pipe($.size());
 });
 
-gulp.task('webserver', function() {
-  return gulp.src(['.tmp', 'app'])
-    .pipe($.webserver({
-      host: '0.0.0.0', //change to 'localhost' to disable outside connections
-      livereload: true,
-      open: true
-    }));
+gulp.task('images', function() {
+  return gulp.src('app/images/**/*')
+    .pipe($.cache($.imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe($.size());
+});
+
+gulp.task('fonts', function() {
+  return gulp.src(['app/fonts/**/*'])
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe($.size());
+});
+
+gulp.task('extras', function () {
+  return gulp.src(['app/*.txt', 'app/*.ico'])
+    .pipe(gulp.dest('dist/'))
+    .pipe($.size());
 });
 
 gulp.task('serve', function() {
-  runSequence('clean:dev', ['scripts', 'styles'], 'webserver');
-
-  gulp.watch('app/*.html');
-
-  gulp.watch('app/scripts/**/*.js', ['scripts']);
-
-  gulp.watch('app/scripts/**/*.jsx', ['scripts']);
-
-  gulp.watch('app/styles/**/*.less', ['styles']);
+  gulp.src('dist')
+    .pipe($.webserver({
+      livereload: true,
+      port: 9000
+    }));
+});
+gulp.task('jest', function () {
+  var nodeModules = path.resolve('./node_modules');
+  return gulp.src('app/scripts/**/__tests__')
+    .pipe($.jest({
+      scriptPreprocessor: nodeModules + '/babel-jest',
+      unmockedModulePathPatterns: [nodeModules + '/react']
+    }));
+});
+gulp.task('set-production', function() {
+  process.env.NODE_ENV = 'production';
 });
 
-gulp.task('build', function() {
-  env = 'prod';
+gulp.task('minify:js', function() {
+  return gulp.src('dist/scripts/**/*.js')
+    .pipe($.uglify())
+    .pipe(gulp.dest('dist/scripts/'))
+    .pipe($.size());
+});
 
-  runSequence(['clean:dev', 'clean:dist'],
-              ['scripts', 'styles', 'imagemin', 'copy'],
-              'bundle');
+gulp.task('minify:css', function() {
+  return gulp.src('dist/styles/**/*.css')
+    .pipe($.minifyCss())
+    .pipe(gulp.dest('dist/styles'))
+    .pipe($.size());
+});
+
+gulp.task('minify', ['minify:js', 'minify:css']);
+
+gulp.task('clean', del.bind(null, 'dist'));
+
+gulp.task('bundle', ['html', 'styles', 'scripts', 'images', 'fonts', 'extras']);
+
+gulp.task('clean-bundle', sync(['clean', 'bundle']));
+
+gulp.task('build', ['clean-bundle'], bundler.stop.bind(bundler));
+
+gulp.task('build:production', sync(['set-production', 'build', 'minify']));
+
+gulp.task('serve:production', sync(['build:production', 'serve']));
+gulp.task('test', ['jest']);
+gulp.task('default', ['build']);
+
+gulp.task('watch', sync(['clean-bundle', 'serve']), function() {
+  bundler.watch();
+  gulp.watch('app/*.html', ['html']);
+  gulp.watch('app/styles/**/*.scss', ['styles']);
+  gulp.watch('app/images/**/*', ['images']);
+  gulp.watch('app/fonts/**/*', ['fonts']);
 });
