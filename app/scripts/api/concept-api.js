83a25618e4240cbb2ac00479dd4f2d2a1052355d
Gregor Weber <gregorwbr@gmail.com>
2015-04-03 17:00:29 +0200

    Fix special URL characters encoding in concept names and correct misspelling of resource

--- app/scripts/api/concept-api.js
+++ app/scripts/api/concept-api.js
@@ -3,7 +3,7 @@ let request = require('superagent');
 let host = require('../configs/api.custom').host;
 
 let endpoint = `${host}/concepts`;
-let ressource = (name) => `${endpoint}/${name}`;
+let resource = (name) => `${endpoint}/${encodeURIComponent(name)}`;
 
 let promiseWrap = (req) => new Promise((resolve) => {
 	req.end((err, res) => resolve(res.body));
@@ -16,7 +16,7 @@ export default {
 	},
 
 	find(name) {
-		return promiseWrap(request.get(ressource(name)));
+		return promiseWrap(request.get(resource(name)));
 	},
 
 	create(concept) {
@@ -24,11 +24,11 @@ export default {
 	},
 
 	update(name, concept) {
-		return promiseWrap(request.post(ressource(name), {concept}));
+		return promiseWrap(request.post(resource(name), {concept}));
 	},
 
 	delete(name) {
-		return promiseWrap(request.del(ressource(name)));
+		return promiseWrap(request.del(resource(name)));
 	},
 
 	reposition(concepts) {
