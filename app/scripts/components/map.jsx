8ef71aa5fb953e9576aee2009ca7ff0074efb57d
Gregor <gregorwbr@gmail.com>
2015-05-03 21:58:13 +0200

    simple req links, no commented out physics

--- app/scripts/components/map.jsx
+++ app/scripts/components/map.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'd3';
 import _ from 'lodash';
-import 'victor';
+import Victor from 'victor';
 
 let MapNavigationMixin = require('./mixins/MapNavigationMixin');
 
@@ -12,7 +12,7 @@ const OUTER_STRENGTH = -.3;
 
 let sqr = n => Math.pow(n, 2);
 
-let D3Map = React.createClass({
+let GraphMap = React.createClass({
 
 	propTypes: {
 		concepts: React.PropTypes.object,
@@ -46,6 +46,7 @@ let D3Map = React.createClass({
 		this.animated = false;
 
 		this.concepts = concepts;
+
 		let containers = this.containers = new Map();
 		for (let [id, concept] of concepts) {
 			let {container} = concept;
@@ -53,34 +54,34 @@ let D3Map = React.createClass({
 			else containers.get(container).push(concept);
 		}
 
-		this.stuff(this.group, containers.get(null));
-
 		let links = [];
-		for (let [id, c] of concepts) for (let req of c.reqs) links.push({
-			'from': concepts.get(req), 'to': c
-		});
+		for (let [id, c] of concepts) for (let req of c.reqs) {
+			links.push({'from': concepts.get(req), 'to': c});
+		}
 
 		this.links = this.group.selectAll('.link')
 			.data(links)
 			.enter().append('line')
 			.attr('class', 'link');
+
+		this.renderLinks();
+		this.createDeepForces(this.group, containers.get(null));
 	},
 
-	stuff(parentEl, concepts) {
+	createDeepForces(parentEl, concepts) {
 		let self = this;
 		if (!concepts || concepts.length == 0) return;
 
-		this.addToForceLayout(parentEl, concepts).each(function (d) {
-			self.stuff(d3.select(this), self.containers.get(d.id));
+		this.createForceLayout(parentEl, concepts).each(function (d) {
+			self.createDeepForces(d3.select(this), self.containers.get(d.id));
 		});
 	},
 
-	addToForceLayout(parentEl, concepts) {
+	createForceLayout(parentEl, concepts) {
 		let force = d3.layout.force().size([WIDTH, HEIGHT]);
 
 		let container = this.concepts.get(concepts[0].container) ||
 			{'absX': 0, 'absY': 0};
-
 
 		let links = [];
 		for (let i = 0; i < concepts.length; i++) {
@@ -98,11 +99,16 @@ let D3Map = React.createClass({
 		let el = parentEl.selectAll('.node').data(concepts).enter()
 			.append('g').attr('class', 'node');
 
+		let onClick = (d => {
+			if (!this.state.wasPanning) this.props.onSelect(d.id);
+		}).bind(this);
 		let circle = el.append('circle')
-			.style({'stroke': d => d.color || 'white', 'fill': '#1f1f1f'});
+			.style({'stroke': d => d.color || 'white', 'fill': 'transparent'})
+			.on('click', onClick);
 			//.call(force.drag);
 
-		circle.append('title').text(d => d.name);
+		circle.append('title')
+			.text(d => d.name).on('click', onClick);
 
 		let label = el.append('text')
 			.text(d => d.name)
@@ -121,30 +127,36 @@ let D3Map = React.createClass({
 						'absX': container.absX - HF_WIDTH + d.x,
 						'absY': container.absY - HF_HEIGHT + d.y
 					});
-					for (let req of d.reqs) {
-						let source = this.concepts.get(req);
-						let x = source.absX - d.absX, y = source.absY - d.absY;
-						let k, sqLength;
-						if (sqLength = x * x + y * y) {
-							let length = Math.sqrt(sqLength);
-							let dist = 40;
-							let multiplier = (e.alpha / 4) * OUTER_STRENGTH * (length - dist) / length;
-							x *= multiplier;
-							y *= multiplier;
-							d.x -= x * (k = 1 / 2);
-							d.y -= y * k;
-							source.x += x * (k = 1 - k);
-							source.y += y * k;
-						}
-					}
+					//for (let i = 0; i < d.reqs.length; i++) {
+					//	let req = d.reqs[i];
+					//	if (!_.isObject(req)) req = d.reqs[i] = {
+					//		'other': this.concepts.get(req), 'lastDists': [], 'targetDist': 50
+					//	};
+					//	let {other, lastDists} = req;
+					//	let x = other.absX - d.absX, y = other.absY - d.absY;
+					//	let k, sqDist;
+					//	if (sqDist = x * x + y * y) {
+					//		let dist = Math.sqrt(sqDist);
+                    //
+					//		//lastDists.push(dist);
+					//		//lastDists = req.lastDists = _.drop(lastDists, lastDists.length - 30);
+                    //
+					//		//let total = lastDists.reduce((n, t) => t + n, 0);
+					//		//let deviation = (total - req.targetDist) / (lastDists.length || 1);
+					//		//if (deviation > 50) req.targetDist += .3;
+					//		//else if (deviation < 30) req.targetDist -= .1;
+                    //
+					//		let distFrac = (dist - req.targetDist) / dist;
+					//		let multiplier = (e.alpha / 4) * OUTER_STRENGTH * distFrac;
+					//		x *= multiplier;
+					//		y *= multiplier;
+					//		d.x -= x * (k = 1 / 2);
+					//		d.y -= y * k;
+					//		req.x += x * (k = 1 - k);
+					//		req.y += y * k;
+					//	}
+					//}
 				});
-
-			this.links.attr({
-					'x1': d => d.from.absX,
-					'y1': d => d.from.absY,
-					'x2': d => d.to.absX,
-					'y2': d => d.to.absY
-			});
 
 			circle.attr('r', d => d.r = BASE_RAD +
 				(!this.containers.has(d.id) ? 0 :
@@ -162,7 +174,7 @@ let D3Map = React.createClass({
 			label.attr('y', d => d.r + 18);
 
 			force
-				.charge(d => -200 + sqr(d.r) * -.18)
+				.charge(d => -200 + sqr(d.r) * -.19)
 				.linkDistance(d => d.source.r + d.target.r + 2 * BASE_RAD)
 				.start();
 
@@ -191,7 +203,34 @@ let D3Map = React.createClass({
 		this.getGroup().attr('transform',
 			`matrix(${scale}, 0, 0, ${scale}, ${pos.x}, ${pos.y})`
 		);
+	},
 
+	renderLinks() {
+		this.links
+			.each(d => {
+				let scale = (v, n) => v.clone().multiply(new Victor(n, n));
+				let fromV = new Victor(d.from.absX, d.from.absY);
+				let toV = new Victor(d.to.absX, d.to.absY);
+
+				let betweenV = fromV.clone().subtract(toV).norm();
+
+				let fromRadV = scale(betweenV, d.from.r);
+				let toRadV = scale(betweenV, d.to.r);
+
+				fromV.subtract(fromRadV);
+				toV.add(toRadV);
+
+				_.assign(d, {
+					'x1': fromV.x, 'y1': fromV.y,
+					'x2': toV.x, 'y2': toV.y
+				});
+			})
+			.attr({
+				'x1': d => d.x1, 'y1': d => d.y1,
+				'x2': d => d.x2, 'y2': d => d.y2
+			});
+
+		window.requestAnimationFrame(this.renderLinks);
 	},
 
 	getGroup() {
@@ -205,4 +244,4 @@ let D3Map = React.createClass({
 
 });
 
-export default D3Map;
+export default GraphMap;
