278b6ba105bcd7cb00827cfda3dd5b0638d56350
Gregor <gregorwbr@gmail.com>
2015-07-21 16:10:12 +0200

    show explanation author & timestamp

--- app/scripts/components/concept-info/explanation-card.jsx
+++ app/scripts/components/concept-info/explanation-card.jsx
@@ -8,7 +8,9 @@ let ExplanationCard = React.createClass({
 
   render() {
     let {explanation, voteDisabled} = this.props;
-    let {content} = explanation;
+    let {content, createdAt} = explanation;
+
+    createdAt = new Date(createdAt).toLocaleString();
 
     if (content && content.trim().startsWith('http')) content = (
       <a className="and-so-on" target="_blank" href={content.trim()}>
@@ -16,22 +18,32 @@ let ExplanationCard = React.createClass({
       </a>
     );
 
-    let votedClass = explanation.hasVoted ? 'voted' : '';
+    let arrowColor = explanation.hasVoted ? 'red' : 'black';
 
     return (
       <div className="explanation card" key={explanation.id}>
-        <div className="row">
+        <div className="row middle-xs">
           <div className="col-xs-1">
             <IconButton iconClassName="icon icon-arrow-up" tooltip="Vote"
                         onClick={() => this.onVoteExplanation(explanation)}
-                        disabled={voteDisabled}
-                        className={`small ${votedClass}`}/>
+                        disabled={voteDisabled} iconStyle={{color: arrowColor}}
+                        className={`small`}/>
           </div>
           <div className="col-xs-1">
             {explanation.votes}
           </div>
           <div className="col-xs-10" style={{'white-space': 'pre'}}
                dangerouslySetInnerHTML={{'__html': content}}/>
+        </div>
+        <div className="row end-xs">
+          <div className="col-xs-6">
+            <div className="row">
+              <div className="col-xs-12">created at <i>{createdAt}</i></div>
+              <div className="col-xs-12">
+                by <em>{explanation.author.name}</em>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
